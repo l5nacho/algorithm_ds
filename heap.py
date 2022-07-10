83972c56799f919769bf9397aff23a77c28465d4
nacho <ignacio.contrerascabello@gmail.com>
2022-07-10 14:36:52 +0200

    Comentarioss y fix_heap_up

--- heap.py
+++ heap.py
@@ -4,6 +4,12 @@ CAPACITY = 10
 # Maximum heap (root node será el mayor)
 class Heap:
     def __init__(self):
+
+        """
+        Size también representa el indice del heap.
+        Heap tiene estructura de arbol pero también se puede
+        representar como un array con indice
+        """
         self.size = 0
         self.heap = [0] * CAPACITY
 
@@ -15,6 +21,28 @@ class Heap:
         self.heap[self.size] = item
         self.size += 1
 
-        # Check heap properties
+        # Check heap properties.
+        # Se mira el elemento anterior. (size - 1)
 
-        self.fix_heap(self.size - 1)
+        self.fix_heap_up(self.size - 1)
+
+    def fix_heap_up(self, index):
+
+        """
+        Cada elemento del heap tiene dos hijos, el valor del indice de
+        cada hijo se calcula de la siguiente manera:
+        Hijo izquierdo -> (indice * 2) + 1
+        Hijo derecho -> (indice * 2) + 2
+        Para buscar el indice del padre del nodo, hay que hacer la operacion
+        inversa al hijo izquierdo y hacer integer division
+        """
+
+        parent_index = (index - 1) // 2
+
+        # Revisamos el heap desde el indice hasta el nodo root para ver si se han violado las
+        # reglas del heap.
+        # Si uno de los elementos es mayor que el padre, se cambian para respetar las reglas.
+
+        if index.size > 0 and self.heap[index] > self.heap[parent_index]:
+            self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+            self.fix_heap_up(parent_index)
