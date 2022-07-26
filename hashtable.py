b100fd65be72429dd43ceb14f792a3f51a4fde90
nacho <ignacio.contrerascabello@gmail.com>
2022-07-26 21:46:39 +0200

    Añadido __init__ y hash_function

---
+++ hashtable.py
@@ -0,0 +1,28 @@
+
+class HashTable:
+
+    def __init__(self):
+        """
+        Dependiendo del load factor, puede que cambie el tamaño de
+        la estructura de datos que subyace el hash table
+        """
+        self.capacity = 10
+        self.keys = [None] * self.capacity
+        self.values = [None] * self.capacity
+
+    def hash_function(self, key):
+        """
+        Tranforma una string a un número con la funcion ord
+        (transforma un caracter a su representacion ASCII)
+        Luego hace modulo con capacity para generar un valor que
+        equivalga a un valor del indice
+        :param key: string
+        :return: integer
+        """
+
+        hash_sum = 0
+
+        for letter in key:
+            hash_sum = hash_sum + ord(letter)
+
+        return hash_sum % self.capacity
