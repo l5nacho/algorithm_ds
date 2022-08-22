717f409cf45cdf974ad67604bb2c4f8131c4f8d9
nacho <ignacio.contrerascabello@gmail.com>
2022-08-22 11:08:49 +0200

    Añadida clase Node

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -5,3 +5,19 @@ class Edge:
         self.weight = weight
         self.start_vertex = start_vertex
         self.target_vertex = target_vertex
+
+class Node:
+
+    def __init__(self, name):
+        self.name = name
+        self.visited = False
+        # El nodo de donde viene el path
+        self.predecessor = None
+        # Lista con los nodos a los que tiene acceso
+        self.adjacency_list = []
+        # Distancia minima desde el source hasta el nodo
+        self.min_distance = float('inf')
+
+    def __lt__(self, other_node):
+        return self.min_distance < other_node.min_distance
+
