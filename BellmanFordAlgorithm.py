3386540c0654ee3a86ca4ccec9a42a62acb12937
nacho <ignacio.contrerascabello@gmail.com>
2022-09-18 13:20:43 +0200

    clase Bellmand Ford - Funcionando

--- BellmanFordAlgorithm.py
+++ BellmanFordAlgorithm.py
@@ -1,5 +1,5 @@
 
-class Vertex:
+class Edge:
 
     def __init__(self, weight, start_vertex, target_vertex):
         self.weight = weight
@@ -12,7 +12,8 @@ class Node:
         self.name = name
         self.adjacency_list = []
         self.predecessor = None
-        self.min_distance = int('inf')
+        self.min_distance = float('inf')
+
 
 class BellmanFordAlgorithm:
 
@@ -31,7 +32,7 @@ class BellmanFordAlgorithm:
                 u = edge.start_vertex
                 v = edge.target_vertex
 
-                dist = edge.start_vertex + edge.weight
+                dist = edge.start_vertex.min_distance + edge.weight
 
                 if dist < v.min_distance:
 
@@ -39,12 +40,12 @@ class BellmanFordAlgorithm:
                     v.predecessor = u
 
         for edge in self.edge_list:
-            if self.check_ciclo(edge):
+            if self.check_cicle(edge):
                 print('Detectado ciclo negativo')
                 return None
 
     def check_cicle(self, edge):
-        if edge.start_vertex + edge.weight < edge.target_vertex:
+        if edge.start_vertex.min_distance + edge.weight < edge.target_vertex.min_distance:
             self.has_cycle = True
             return True
         else:
@@ -60,7 +61,55 @@ class BellmanFordAlgorithm:
             while node is not None:
                 node_list.append(node.name)
                 node = node.predecessor
+            print(" -> ".join(node_list))
 
         else:
             print('ERROR: Hay un ciclo negativo')
+
+if __name__ == '__main__':
+
+    # we have to create the vertices
+    node1 = Node("A")
+    node2 = Node("B")
+    node3 = Node("C")
+    node4 = Node("D")
+    node5 = Node("E")
+    node6 = Node("F")
+    node7 = Node("G")
+
+    # let's create the edges
+    edge1 = Edge(5, node1, node2)
+    edge2 = Edge(9, node1, node5)
+    edge3 = Edge(4, node2, node5)
+    edge4 = Edge(12, node2, node3)
+    edge5 = Edge(7, node2, node4)
+    edge6 = Edge(3, node3, node4)
+    edge7 = Edge(1, node3, node6)
+    edge8 = Edge(9, node4, node7)
+    edge9 = Edge(6, node5, node3)
+    edge10 = Edge(4, node5, node6)
+    edge11 = Edge(2, node6, node7)
+    edge12 = Edge(6, node7, node3)
+
+    # handle the adjacency lists
+    node1.adjacency_list.append(edge1)
+    node1.adjacency_list.append(edge2)
+    node2.adjacency_list.append(edge3)
+    node2.adjacency_list.append(edge4)
+    node2.adjacency_list.append(edge5)
+    node3.adjacency_list.append(edge6)
+    node3.adjacency_list.append(edge7)
+    node4.adjacency_list.append(edge8)
+    node5.adjacency_list.append(edge9)
+    node5.adjacency_list.append(edge10)
+    node6.adjacency_list.append(edge11)
+    node7.adjacency_list.append(edge12)
+
+    # run the algorithm
+    vertices = (node1, node2, node3, node4, node5, node6, node7)
+    edges = (edge1, edge2, edge3, edge4, edge5, edge6, edge7, edge8, edge9, edge10, edge11, edge12)
+
+    algorithm = BellmanFordAlgorithm(vertices, edges, node1)
+    algorithm.find_shortest_path()
+    algorithm.get_shortest_path(node7)
 
