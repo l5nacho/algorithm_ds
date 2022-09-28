2b3a4b3336033f1ea44b645a691176869248e249
nacho <ignacio.contrerascabello@gmail.com>
2022-09-28 19:52:10 +0200

    Disjoint set class find y make_sets

--- KruskalAlgorithm.py
+++ KruskalAlgorithm.py
@@ -19,8 +19,50 @@ class Edge:
 
 class Node:
     # Nodos en la representacion del arbol
-    def __init__(self, rank, node_id, parent):
+    def __init__(self, rank, node_id, parent=None):
 
         self.rank = rank
         self.node_id = node_id
         self.parent = parent
+
+class DisjointSet:
+
+    def __init__(self, vertex_list):
+        self.vertex_list = vertex_list
+        #Lista de los representantes
+        self.root_nodes = []
+        self.make_sets()
+
+    def find(self, node):
+
+        current_node = node
+
+        # Iteramos hasta llegar al representante (root node)
+        while current_node.parent is not None:
+            current_node = current_node.parent
+
+        # Aplicamos path compression
+        # Asignamos el current node a la variable root
+        # Volvemos a asignar current_node a node
+        root = current_node
+        current_node = node
+
+        # Todos los nodos tienen que apuntar al root (path compression)
+        while current_node is not root:
+            temp = current_node.parent
+            current_node.parent = root
+            current_node = temp
+
+
+
+
+
+
+
+    def make_sets(self):
+
+        for v in self.vertex_list:
+            node = Node(0, len(self.root_nodes))
+            v.node = node
+            self.root_nodes.append(node)
+
